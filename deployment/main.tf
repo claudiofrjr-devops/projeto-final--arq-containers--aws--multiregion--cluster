70fee3883d70b9363db6cab79af192870592fd7a
claudio fernandes <claudio.fe.rejes@hotmail.com>
2025-01-05 23:55:09 -0300

    Feat: Cluster Multi-Region

---
+++ deployment/main.tf
@@ -0,0 +1,10 @@
+module "cluster" {
+  source = "../../01-modulos_terraform_aws/projeto-final--arq-containers--modulos-terraform/cluster"
+
+  project_name = var.project_name
+  region       = var.region
+
+  vpc_id          = data.aws_ssm_parameter.vpc.value
+  private_subnets = data.aws_ssm_parameter.private_subnets[*].value
+  public_subnets  = data.aws_ssm_parameter.public_subnets[*].value
+}
